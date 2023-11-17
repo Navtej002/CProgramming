6be4dd976b434bd5b4f21ff2f2a4a0851c7efa92
Navtej Saini <navtejs002@gmail.com>
2023-11-17 16:25:35 -0500

    Fixed unnecessary error check in text file section. Fixed any style issues and polished source file with comments.

--- f7.cpp
+++ f7.cpp
@@ -1,10 +1,11 @@
-//
-//File name: f7.cpp
-//Project Name:
-//Programmers Name:
-//Date of First Revision:
-//Description:
-//
+/*
+* File name: f7.cpp
+* Project Name: Focused Assignment 7
+* Programmers Name: Navtej Saini
+* Date of First Revision: Nov 13, 2023
+* Description: This program creates a text file through fopen() called myTextFile.txt and writes lines of text to it. Then, it creates 
+* a text binary file using the same method and writes in binary the elements of kBinaryData array to it. 
+*/
 
 #include <stdio.h>
 #pragma warning(disable: 4996)   //for fopen() compiler warning
@@ -14,10 +15,10 @@
 int main(void)
 {
     //Text file section
-    FILE* textFile = fopen("myTextFile.txt", "w");  // creates a text file called myTextFile
+    FILE* textFile = fopen("myTextFile.txt", "w");  //Creates a text file called myTextFile
     if (textFile != NULL)
     {
-        if (fputs("This is the first line.\nThis is the second line.\n", textFile) != NULL)  // Write lines to text file
+        if (fputs("This is the first line.\nThis is the second line.\n", textFile) != NULL)  // Writes lines to text file
         {
             printf("Writing to text file failed.\n");
 
@@ -28,15 +29,8 @@ int main(void)
 
             return 1;
         }
-
-        if (fclose(textFile) != NULL)
-        {
-            printf("Closing text file failed.\n");
-            return 1;
-        }
     }
-    else
-    {
+    else {
         printf("Opening text file failed.\n");
         return 1;
     }
@@ -48,8 +42,9 @@ int main(void)
     FILE* binaryFile = fopen("myBinaryFile.data", "wb");      //"wb" formatting code for writing to binary .data file
     if (binaryFile != NULL)
     {
-        size_t elements_written = fwrite(kBinaryData, sizeof(unsigned short), sizeof(kBinaryData) / sizeof(unsigned short), binaryFile);
-        if (elements_written != sizeof(kBinaryData) / sizeof(unsigned short)) //must divide the total size of the array by the size of each element to determine how many elements in the kBinaryData array
+        size_t elementsWritten = fwrite(kBinaryData, sizeof(unsigned short), sizeof(kBinaryData) / sizeof(unsigned short), binaryFile); //Declares elementsWritten as unsigned int variable to store how many elements wrote to binary file. 
+
+        if (elementsWritten != sizeof(kBinaryData) / sizeof(unsigned short)) //Must divide the total size of the array by the size of each element to determine how many elements in the kBinaryData array
         {
             printf("Writing to binary file failed.\n");
 
@@ -72,10 +67,6 @@ int main(void)
         return 1;
     }
     
-
-
-
-
 
     return 0;
 }
