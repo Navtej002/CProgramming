bb0937d853dbf30164ca693b9a2982041994230a
Navtej Saini <navtejs002@gmail.com>
2023-11-16 20:37:38 -0500

    added error checking to fclose() and added prompt for error checking if the file opened successfully or not

--- f7.cpp
+++ f7.cpp
@@ -16,12 +16,22 @@ int main(void)
     FILE* textFile = fopen("myTextFile.txt", "w");  //creates a text file called myTextFile
     if (textFile != NULL) 
     {
+
         if (fputs("This is line 1.\nThis is line 2.\n", textFile) != NULL)  // Write lines to text file
         {
             printf("Writing to text file failed.\n");
-      
+            if (fclose(textFile) != 0);
+            {
+                printf("Closing text file failed.");
+            }
+
             return 1;
         }
+        else {
+            printf("Opening text file failed.\n");
+            return 1;
+        }
+
     }
     
         
