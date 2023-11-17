786ef1f733a15b00ca1296efcc1ee281732204c1
Navtej Saini <navtejs002@gmail.com>
2023-11-17 16:01:22 -0500

    Completed logic for creating and writing to binary file. Error checking for file functions also implemented. Added some inline comments.

--- f7.cpp
+++ f7.cpp
@@ -7,10 +7,13 @@
 //
 
 #include <stdio.h>
-#pragma warning(disable: 4996)
+#pragma warning(disable: 4996)   //for fopen() compiler warning
+
+
 
 int main(void)
 {
+    //Text file section
     FILE* textFile = fopen("myTextFile.txt", "w");  // creates a text file called myTextFile
     if (textFile != NULL)
     {
@@ -42,12 +45,33 @@ int main(void)
     //Binary file section
     const unsigned short kBinaryData[] = { 26946, 24942, 31090, 25632, 29793, 8289, 28518, 8306, 28537, 33141, 39308 };
 
-    FILE* binaryFile = fopen("myBinaryFile.txt", "wb");
+    FILE* binaryFile = fopen("myBinaryFile.data", "wb");      //"wb" formatting code for writing to binary .data file
     if (binaryFile != NULL)
     {
+        size_t elements_written = fwrite(kBinaryData, sizeof(unsigned short), sizeof(kBinaryData) / sizeof(unsigned short), binaryFile);
+        if (elements_written != sizeof(kBinaryData) / sizeof(unsigned short)) //must divide the total size of the array by the size of each element to determine how many elements in the kBinaryData array
+        {
+            printf("Writing to binary file failed.\n");
+
+            if (fclose(binaryFile) != NULL)
+            {
+                printf("Closing binary file failed.\n");
+            }
+
+            return 1;
+        }
+
+        if (fclose(binaryFile) != NULL)
+        {
+            printf("Closing binary file failed.\n");
+            return 1;
+        }
+    }
+    else {
         printf("Opening binary file failed.");
         return 1;
     }
+    
 
 
 
