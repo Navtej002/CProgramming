f835b9ef0b07e46e90ee29ad1faf5464a54cbf6a
Navtej Saini <navtejs002@gmail.com>
2023-11-16 20:10:21 -0500

    completed functionality of creating and writing to a file with error checking.

---
+++ f7.cpp
@@ -0,0 +1,32 @@
+//
+//File name: f7.cpp
+//Project Name:
+//Programmers Name:
+//Date of First Revision:
+//Description:
+//
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+
+int main(void)
+{
+
+
+    FILE* textFile = fopen("myTextFile.txt", "w");  //creates a text file called myTextFile
+    if (textFile != NULL) 
+    {
+        if (fputs("This is line 1.\nThis is line 2.\n", textFile) != NULL)  // Write lines to text file
+        {
+            printf("Writing to text file failed.\n");
+      
+            return 1;
+        }
+    }
+    
+        
+
+
+
+	return 0;
+}
