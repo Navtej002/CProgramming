32ee7515cf3a97614ffe618dbd9f30e0edf16eff
Navtej Saini <navtejs002@gmail.com>
2023-11-17 14:27:50 -0500

    fixed logic with writing to file and error checking all file fucntions. Got started on the Binary file portion.~

--- f7.cpp
+++ f7.cpp
@@ -11,32 +11,49 @@
 
 int main(void)
 {
-
-
-    FILE* textFile = fopen("myTextFile.txt", "w");  //creates a text file called myTextFile
-    if (textFile != NULL) 
+    FILE* textFile = fopen("myTextFile.txt", "w");  // creates a text file called myTextFile
+    if (textFile != NULL)
     {
-
-        if (fputs("This is line 1.\nThis is line 2.\n", textFile) != NULL)  // Write lines to text file
+        if (fputs("This is the first line.\nThis is the second line.\n", textFile) != NULL)  // Write lines to text file
         {
             printf("Writing to text file failed.\n");
-            if (fclose(textFile) != 0);
+
+            if (fclose(textFile) != NULL)
             {
-                printf("Closing text file failed.");
+                printf("Closing text file failed.\n");
             }
 
             return 1;
         }
-        else {
-            printf("Opening text file failed.\n");
+
+        if (fclose(textFile) != NULL)
+        {
+            printf("Closing text file failed.\n");
             return 1;
         }
-
     }
-    
-        
+    else
+    {
+        printf("Opening text file failed.\n");
+        return 1;
+    }
+
+
+    //Binary file section
+    const unsigned short kBinaryData[] = { 26946, 24942, 31090, 25632, 29793, 8289, 28518, 8306, 28537, 33141, 39308 };
+
+    FILE* binaryFile = fopen("myBinaryFile.txt", "wb");
+    if (binaryFile != NULL)
+    {
+        printf("Opening binary file failed.");
+        return 1;
+    }
 
 
 
-	return 0;
+
+
+    return 0;
 }
+
+
