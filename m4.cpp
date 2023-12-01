9c94e51ac8f73a28f26b66502d62c2fe329cc010
Navtej Saini <navtejs002@gmail.com>
2023-11-30 20:14:12 -0500

    Implemented logic for deciding which if primaryTeam won against opposingTeam. Began developing the parseLine() function by first looking for a comma using strchr() and parseLine()'s parameters. Added prototypes for processGames() and parseLine() and defined constant for max line length

--- m4.cpp
+++ m4.cpp
@@ -7,13 +7,23 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+
+
 #pragma warning(disable: 4996)
 
 #define MAX_FILE_LENGTH 41
+#define MAX_LINE_LENGTH 41
+
+
+int parseLine(char* wholeLine, char* teamName, int* teamScore, int* oppScore);
+int processGames(const char* filename);
+
+
 
 int main(void)
 {
-
+	int status = 0;
 	char filename[MAX_FILE_LENGTH];
 
 	FILE* teamsFile = fopen("teams.txt", "r");
@@ -23,12 +33,26 @@ int main(void)
 		return 1;
 	}
 
+	while (fgets(filename, sizeof(filename), teamsFile) != NULL)
+	{
+		
+
+		int status = processGames(filename);
+
+		if (status != NULL)
+		{
+			printf("Error processing %s\n", filename);
+		}
+
+
+	}
+
 
 	return 0;
 }
 
 
-int parseGames(const char* filename)
+int processGames(const char* filename)
 {
 	int wins = 0;   //Counter for wins
 	int losses = 0;  //Counter for losses
@@ -47,10 +71,62 @@ int parseGames(const char* filename)
 	printf("Processing %s:\n", filename);
 
 
+	char gameLine[MAX_LINE_LENGTH];
+
+	char primaryTeam[MAX_FILE_LENGTH];
+	char opposingTeam[MAX_FILE_LENGTH];
+
+
+	strncpy(primaryTeam, filename, strlen(filename) - 4);
+	primaryTeam[strlen(filename) - 4] = '\0';  // Remove ".txt" extension and ensure it null-terminates
+
+
+	while (fgets(gameLine, sizeof(gameLine), gameFile) != NULL)
+	{
+		gameLine[strcspn(gameLine, "\n")] = 0;   //removes closing sequence "\n" if present
+
+		int status = parseLine(gameLine, opposingTeam, &teamScore, &oppScore);
+
+		if (status == 0)
+		{
+			printf("\t%s", primaryTeam);
+
+			if (teamScore > oppScore)
+			{
+				printf("beat %s %d-%d\n", opposingTeam, teamScore, oppScore);
+				wins++;  //increments count of wins by 1
+			}
+			else if (teamScore < oppScore) {
+				printf("lost to %s %d-%d\n", opposingTeam, teamScore, oppScore);
+				losses++;  //increments count of losses by 1
+			}
+			else {
+				printf("tied with %s %d-%d\n", opposingTeam, teamScore, oppScore);
+				ties++;
+			}
+		}
+	}
+
+
+
+
+    return 0;
+}
 
 
 
 
 
+int parseLine(char* wholeLine, char* teamName, int* teamScore, int* oppScore)
+{
+    char* comma = strchr(wholeLine, ',');
+    if (comma == NULL)
+    {
+        printf("Error: Missing comma in line\n");
+        return 1;
+    }
+
+    
+    return 0;
 
 }
